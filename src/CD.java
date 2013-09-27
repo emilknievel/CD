762d05bf5740bdb79736e7372b6aaaa00abfa069
emilknievel <emil.vagstedt@gmail.com>
2013-09-27 08:25:27 +0200

    Initial commit

---
+++ src/CD.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA. User: Emil Date: 2013-09-27 Time: 08:21 To change this template use File | Settings | File
+ * Templates.
+ */
+public class CD
+{
+    public static void main(String[] args) {
+
+    }
+}
